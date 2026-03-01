222a715095bf2746a2e5c995feb3da3380c130bc
sudharsanaa12 <sa0683@srmist.edu.in>
2026-03-01 11:34:29 +0530

    initial java files

---
+++ U3.java
@@ -0,0 +1,55 @@
+
+public class U3{
+
+    public static void main(String[] args) {
+
+        System.out.println(String.join(" ",
+                "  *****  ",
+                "  *****  ",
+                " ******  ",
+                "  *****  "
+        ));
+
+        System.out.println(String.join(" ",
+                " *     * ",
+                " *     * ",
+                "*     *  ",
+                " *       "
+        ));
+
+        System.out.println(String.join(" ",
+                " *     * ",
+                " *     * ",
+                "*     *  ",
+                " *       "
+        ));
+
+        System.out.println(String.join(" ",
+                " *     * ",
+                " *     * ",
+                " ******  ",
+                "  *****  "
+        ));
+
+        System.out.println(String.join(" ",
+                " *     * ",
+                " *     * ",
+                "*        ",
+                "       * "
+        ));
+
+        System.out.println(String.join(" ",
+                " *     * ",
+                " *     * ",
+                "*        ",
+                "*      * "
+        ));
+
+        System.out.println(String.join(" ",
+                "  *****  ",
+                "  *****  ",
+                "*        ",
+                " ******  "
+        ));
+    }
+}
